2c9e9ae25e1e043e905b70a5924388a3ab936954
Juan P. Giraldo <juan>
2015-09-03 02:05:47 -0500

    Configurations for run correctly

--- Cool.Module.Service/Startup.cs
+++ Cool.Module.Service/Startup.cs
@@ -1,20 +1,22 @@
 ﻿using System.Web.Http;
+using Cool.Module.Service;
 using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
 
+[assembly: OwinStartup(typeof(Startup))]
+
 namespace Cool.Module.Service
 {
-    [assembly: OwinStartup(typeof(Startup))]
     public class Startup
     {
         public void Configuration(IAppBuilder app)
         {
             var config = new HttpConfiguration();
-            
+
             config.MapHttpAttributeRoutes();
             app.UseCors(CorsOptions.AllowAll);
-            
+
             app.UseWebApi(config);
         }
     }
