5a88ac5aed0dcae09ec73f125094098e08dae952
Juan P. Giraldo <juan>
2015-09-03 01:39:16 -0500

    Base project

---
+++ Cool.Module.Service/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+
+namespace Cool.Module.Service
+{
+    [assembly: OwinStartup(typeof(Startup))]
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+            
+            config.MapHttpAttributeRoutes();
+            app.UseCors(CorsOptions.AllowAll);
+            
+            app.UseWebApi(config);
+        }
+    }
+}
